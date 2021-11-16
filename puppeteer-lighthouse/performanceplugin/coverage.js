a232b42ce01213b7e6cb20370521e329e5cec850
winsonye <winsonye@tencent.com>
2021-11-16 12:29:04 +0800

    demo

--- puppeteer-lighthouse/performanceplugin/coverage.js
+++ puppeteer-lighthouse/performanceplugin/coverage.js
@@ -1,6 +1,6 @@
 
- const pti = require('puppeteer-to-istanbul');
-
+const pti = require('puppeteer-to-istanbul');
+//
 module.exports = async function coverageStat(page, url) {
     // 启用 JavaScript 和 CSS 覆盖
     await Promise.all([page.coverage.startCSSCoverage(), page.coverage.startJSCoverage()]);
@@ -17,9 +17,9 @@ module.exports = async function coverageStat(page, url) {
     let usedBytes = 0;
     const coverage = [...jsCoverage, ...cssCoverage];
     for (const entry of coverage) {
-    totalBytes += entry.text.length;
-    for (const range of entry.ranges)
-        usedBytes += range.end - range.start - 1;
+        totalBytes += entry.text.length;
+        for (const range of entry.ranges)
+            usedBytes += range.end - range.start - 1;
     }
     // await pti.write([...jsCoverage, ...cssCoverage], { includeHostname: true , storagePath: './.nyc_output' })
 
